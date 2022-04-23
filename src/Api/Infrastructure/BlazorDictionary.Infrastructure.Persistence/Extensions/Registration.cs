9a1d5308e4ad77fc2fce8de9b57c940f7f78a75f
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-04-24 02:49:24 +0300

    BlazorDictionary.Infrastructure.Persistence.Extensions Registration added

---
+++ src/Api/Infrastructure/BlazorDictionary.Infrastructure.Persistence/Extensions/Registration.cs
@@ -0,0 +1,25 @@
+﻿using BlazorDictionary.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorDictionary.Infrastructure.Persistence.Extensions;
+
+public static class Registration
+{
+    public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BlazorDictionaryDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), opt =>
+            {
+                opt.EnableRetryOnFailure();
+            });
+        });
+
+        //var seedData = new SeedData();
+        //seedData.SeedAsync(configuration).GetAwaiter().GetResult();
+
+        return services;
+    }
+}
