33551f66007a59cc486749bde4289d1ae3cf12d4
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-04-24 23:17:08 +0300

    BlazorDictionary.Infrastructure.Persistence.Repositories EntryRepository added

---
+++ src/Api/Infrastructure/BlazorDictionary.Infrastructure.Persistence/Repositories/EntryRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlazorDictionary.Api.Application.Interfaces.Repositories;
+using BlazorDictionary.Api.Domain.Models;
+using BlazorDictionary.Infrastructure.Persistence.Contexts;
+
+namespace BlazorDictionary.Infrastructure.Persistence.Repositories;
+
+public class EntryRepository : BaseRepository<Entry>, IEntryRepository
+{
+    public EntryRepository(BlazorDictionaryDbContext context) : base(context) { }
+}
