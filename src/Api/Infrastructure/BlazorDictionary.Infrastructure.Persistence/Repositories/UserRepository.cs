03dde63e67f385bcf901fb3013dd492c8225d325
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-04-24 23:17:21 +0300

    BlazorDictionary.Infrastructure.Persistence.Repositories UserRepository added

---
+++ src/Api/Infrastructure/BlazorDictionary.Infrastructure.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlazorDictionary.Api.Application.Interfaces.Repositories;
+using BlazorDictionary.Api.Domain.Models;
+using BlazorDictionary.Infrastructure.Persistence.Contexts;
+
+namespace BlazorDictionary.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    public UserRepository(BlazorDictionaryDbContext context) : base(context) { }
+}
