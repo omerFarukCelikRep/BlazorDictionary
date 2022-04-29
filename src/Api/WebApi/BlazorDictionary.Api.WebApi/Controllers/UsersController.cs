47d67a6b7e0f13c12b6cea9757228829400d2c30
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-04-29 11:37:24 +0300

    BlazorDictionary.Api.WebApi.Controllers UsersController added

---
+++ src/Api/WebApi/BlazorDictionary.Api.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using BlazorDictionary.Common.Models.RequestModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDictionary.Api.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginUserCommand loginUserCommand)
+    {
+        var response = _mediator.Send(loginUserCommand);
+
+        return Ok(response);
+    }
+}
