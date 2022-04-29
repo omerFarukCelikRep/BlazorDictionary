53dccb2b6bc46d20870852b82e54e271dc64cc25
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-04-29 11:35:31 +0300

    BlazorDictionary.Api.Application.Mapping UserMappingProfile added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Mapping/UserMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BlazorDictionary.Api.Domain.Models;
+using BlazorDictionary.Common.Models.Queries;
+
+namespace BlazorDictionary.Api.Application.Mapping;
+
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<User, LoginUserViewModel>()
+            .ReverseMap();
+    }
+}
