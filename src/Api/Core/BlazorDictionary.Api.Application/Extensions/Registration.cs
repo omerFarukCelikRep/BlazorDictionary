7aa02028656b54cc5db52ac2106e558e3e4ff7cd
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-04-29 11:34:31 +0300

    BlazorDictionary.Api.Application.Extensions Registration created

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Extensions/Registration.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BlazorDictionary.Api.Application.Extensions;
+
+public static class Registration
+{
+    public static IServiceCollection AddApplicationRegistration(this IServiceCollection services)
+    {
+        var assemblies = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(assemblies);
+
+        services.AddAutoMapper(assemblies);
+
+        services.AddValidatorsFromAssembly(assemblies);
+
+        return services;
+    }
+}
