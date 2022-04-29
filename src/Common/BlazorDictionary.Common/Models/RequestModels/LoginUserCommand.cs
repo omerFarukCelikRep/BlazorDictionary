8924ef3588c7c522ec9bb6bb22620fb2d9073c27
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-04-29 11:39:09 +0300

    BlazorDictionary.Common.Models.RequestModels LoginUserCommand added

---
+++ src/Common/BlazorDictionary.Common/Models/RequestModels/LoginUserCommand.cs
@@ -0,0 +1,21 @@
+﻿using BlazorDictionary.Common.Models.Queries;
+using MediatR;
+
+namespace BlazorDictionary.Common.Models.RequestModels;
+
+public class LoginUserCommand : IRequest<LoginUserViewModel>
+{
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+
+    public LoginUserCommand(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+
+    public LoginUserCommand()
+    {
+
+    }
+}
