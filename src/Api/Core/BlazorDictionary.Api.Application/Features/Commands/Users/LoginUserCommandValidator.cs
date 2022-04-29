81c0ada8762c15362ae331d9f089629d0ae28afd
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-04-29 11:35:17 +0300

    BlazorDictionary.Api.Application.Features.Commands.Users LoginUserCommandValidator created

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/Users/LoginUserCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using BlazorDictionary.Common.Models.RequestModels;
+using FluentValidation;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.Users;
+
+public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+{
+    public LoginUserCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotNull().WithMessage("{PropertyName} cannot be null")
+            .NotEmpty().WithMessage("{PropertyName} cannot be empty")
+            .EmailAddress(FluentValidation.Validators.EmailValidationMode.AspNetCoreCompatible).WithMessage("{PropertyName} not a valid email address");
+
+        RuleFor(x => x.Password)
+             .NotNull().WithMessage("{PropertyName} cannot be null")
+             .NotEmpty().WithMessage("{PropertyName} cannot be empty")
+             .MinimumLength(6).WithMessage("{PropertyName} should at least be {MinLength} characters");
+    }
+}
