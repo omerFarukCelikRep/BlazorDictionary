09e48a327443c4dcbab9460722b5ee9398b74a76
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 02:13:51 +0300

    BlazorDictionary.Common.Models.RequestModels UpdateUserCommand added

---
+++ src/Common/BlazorDictionary.Common/Models/RequestModels/UpdateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Common.Models.RequestModels;
+
+public class UpdateUserCommand : IRequest<Guid>
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+}
