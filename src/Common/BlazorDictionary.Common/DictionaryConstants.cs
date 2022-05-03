ca51e51478a7184031b1de6837d526707adab679
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 02:14:07 +0300

    BlazorDictionary.Common DictionaryConstants added

---
+++ src/Common/BlazorDictionary.Common/DictionaryConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorDictionary.Common;
+
+public class DictionaryConstants
+{
+    public const string RabbitMqHost = "localhost";
+    public const string DefaultExchangeType = "direct";
+
+    public const string UserExhangeName = "UserExchange";
+    public const string UserEmailChangedQueueName = "UserEmailChangedQueue";
+}
