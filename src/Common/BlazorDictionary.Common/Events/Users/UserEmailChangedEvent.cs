49e484d7d5a1e3eb533f475dd1c93c292f612038
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 02:13:08 +0300

    BlazorDictionary.Common.Events.Users UserEmailChangedEvent added

---
+++ src/Common/BlazorDictionary.Common/Events/Users/UserEmailChangedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorDictionary.Common.Events.Users;
+
+public class UserEmailChangedEvent
+{
+    public string OldEmailAddress { get; set; }
+    public string NewEmailAddress { get; set; }
+}
