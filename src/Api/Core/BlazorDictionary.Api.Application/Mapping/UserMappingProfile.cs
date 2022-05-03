47d03a9ce8f209773fb55b90c17e2b5a66945a1c
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 02:12:11 +0300

    CreateUserCommand and UpdateUserCommand mappings added

--- src/Api/Core/BlazorDictionary.Api.Application/Mapping/UserMappingProfile.cs
+++ src/Api/Core/BlazorDictionary.Api.Application/Mapping/UserMappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BlazorDictionary.Api.Domain.Models;
 using BlazorDictionary.Common.Models.Queries;
+using BlazorDictionary.Common.Models.RequestModels;
 
 namespace BlazorDictionary.Api.Application.Mapping;
 
@@ -9,6 +10,12 @@ public class UserMappingProfile : Profile
     public UserMappingProfile()
     {
         CreateMap<User, LoginUserViewModel>()
+            .ReverseMap();
+
+        CreateMap<User, CreateUserCommand>()
+            .ReverseMap();
+
+        CreateMap<User, UpdateUserCommand>()
             .ReverseMap();
     }
 }
