50259ffa5786a2c10ff3cee434f7bff62c46f970
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 02:13:38 +0300

    BlazorDictionary.Common.Models.RequestModels CreateUserCommand added

---
+++ src/Common/BlazorDictionary.Common/Models/RequestModels/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Common.Models.RequestModels;
+
+public class CreateUserCommand : IRequest<Guid>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
