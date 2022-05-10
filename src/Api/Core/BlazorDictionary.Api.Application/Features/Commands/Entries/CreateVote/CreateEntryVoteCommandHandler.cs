0d6059b78263e2f6d24652e1f598ee9bb9cbc1b3
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:39:20 +0300

    BlazorDictionary.Api.Application.Features.Commands.Entries.CreateVote CreateEntryVoteCommandHandler added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/Entries/CreateVote/CreateEntryVoteCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using BlazorDictionary.Common;
+using BlazorDictionary.Common.Events.Entries;
+using BlazorDictionary.Common.Infrastracture;
+using BlazorDictionary.Common.Models.RequestModels;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.Entries.CreateVote;
+
+public class CreateEntryVoteCommandHandler : IRequestHandler<CreateEntryVoteCommand, bool>
+{
+    public async Task<bool> Handle(CreateEntryVoteCommand request, CancellationToken cancellationToken)
+    {
+        QueueFactory.SendMessageToExhange(exchangeName: DictionaryConstants.VoteExhangeName,
+                                         exchangeType: DictionaryConstants.DefaultExchangeType,
+                                         queueName: DictionaryConstants.CreateEntryVoteQueueName,
+                                         obj: new CreateEntryVoteEvent()
+                                         {
+                                             EntryId = request.EntryId,
+                                             CreatedBy = request.CreatedBy,
+                                             VoteType = request.VoteType
+                                         });
+
+        return await Task.FromResult(true);
+    }
+}
