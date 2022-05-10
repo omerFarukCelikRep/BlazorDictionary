9522ad7d90eef1bac2cb2d82f3784eabd26868b0
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:40:03 +0300

    BlazorDictionary.Api.Application.Features.Commands.Entries.DeleteVote DeleteEntryVoteCommand added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/Entries/DeleteVote/DeleteEntryVoteCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.Entries.DeleteVote;
+
+public class DeleteEntryVoteCommand : IRequest<bool>
+{
+    public DeleteEntryVoteCommand(Guid entryId, Guid userId)
+    {
+        EntryId = entryId;
+        UserId = userId;
+    }
+
+    public Guid EntryId { get; set; }
+    public Guid UserId { get; set; }
+}
