f2f3253624cabf3ba0329a7cc5141a798abb097c
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:39:35 +0300

    BlazorDictionary.Api.Application.Features.Commands.Entries.DeleteFav DeleteEntryFavCommand added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/Entries/DeleteFav/DeleteEntryFavCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.Entries.DeleteFav;
+
+public class DeleteEntryFavCommand : IRequest<bool>
+{
+    public DeleteEntryFavCommand(Guid entryId, Guid userId)
+    {
+        EntryId = entryId;
+        UserId = userId;
+    }
+
+    public Guid EntryId { get; set; }
+    public Guid UserId { get; set; }
+}
