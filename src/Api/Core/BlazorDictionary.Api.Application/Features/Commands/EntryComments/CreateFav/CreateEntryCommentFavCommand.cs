4735e34a18693e4f16516e5f0610dee863ae7f9e
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:40:45 +0300

    BlazorDictionary.Api.Application.Features.Commands.EntryComments.CreateFav CreateEntryCommentFavCommand added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/EntryComments/CreateFav/CreateEntryCommentFavCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.EntryComments.CreateFav;
+
+public class CreateEntryCommentFavCommand : IRequest<bool>
+{
+    public CreateEntryCommentFavCommand(Guid entryCommentId, Guid userId)
+    {
+        EntryCommentId = entryCommentId;
+        UserId = userId;
+    }
+
+    public Guid EntryCommentId { get; set; }
+    public Guid UserId { get; set; }
+}
