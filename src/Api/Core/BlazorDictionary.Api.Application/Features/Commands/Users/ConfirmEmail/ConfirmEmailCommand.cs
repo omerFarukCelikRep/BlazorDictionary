729ccfcb8068bfe1907410ae1329c4e3acd32320
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:48:22 +0300

    BlazorDictionary.Api.Application.Features.Commands.Users.ConfirmEmail ConfirmEmailCommand added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/Users/ConfirmEmail/ConfirmEmailCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.Users.ConfirmEmail;
+
+public class ConfirmEmailCommand : IRequest<bool>
+{
+    public Guid ConfirmationId { get; set; }
+
+}
