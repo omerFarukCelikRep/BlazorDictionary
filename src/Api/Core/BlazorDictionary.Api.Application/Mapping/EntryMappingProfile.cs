c3793f97bcfa6381b667047b8139968892a90e35
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:55:31 +0300

    BlazorDictionary.Api.Application.Mapping EntryMappingProfile added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Mapping/EntryMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BlazorDictionary.Api.Domain.Models;
+using BlazorDictionary.Common.Models.RequestModels;
+
+namespace BlazorDictionary.Api.Application.Mapping;
+
+public class EntryMappingProfile : Profile
+{
+    public EntryMappingProfile()
+    {
+        CreateMap<Entry, CreateEntryCommand>()
+            .ReverseMap();
+
+        CreateMap<EntryComment, CreateEntryCommentCommand>()
+            .ReverseMap();
+    }
+}
