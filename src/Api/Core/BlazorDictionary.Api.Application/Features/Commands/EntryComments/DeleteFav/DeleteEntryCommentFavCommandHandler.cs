a10467629370b9dde473189c4cb30db0c88f147e
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:47:14 +0300

    BlazorDictionary.Api.Application.Features.Commands.EntryComments.DeleteFav DeleteEntryCommentFavCommandHandler added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/EntryComments/DeleteFav/DeleteEntryCommentFavCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using BlazorDictionary.Common;
+using BlazorDictionary.Common.Events.EntryComments;
+using BlazorDictionary.Common.Infrastracture;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.EntryComments.DeleteFav;
+
+public class DeleteEntryCommentFavCommandHandler : IRequestHandler<DeleteEntryCommentFavCommand, bool>
+{
+    public async Task<bool> Handle(DeleteEntryCommentFavCommand request, CancellationToken cancellationToken)
+    {
+        QueueFactory.SendMessageToExhange(exchangeName: DictionaryConstants.FavExhangeName,
+                                            exchangeType: DictionaryConstants.DefaultExchangeType,
+                                            queueName: DictionaryConstants.DeleteEntryCommentFavQueueName,
+                                            obj: new DeleteEntryCommentFavEvent()
+                                            {
+                                                EntryCommentId = request.EntryCommentId,
+                                                CreatedBy = request.UserId
+                                            });
+
+        return await Task.FromResult(true);
+    }
+}
