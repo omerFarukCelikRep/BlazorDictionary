9e8f10369ad61408b0896931ba4b33cb7f08f2ea
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 01:03:53 +0300

    constants added

--- src/Common/BlazorDictionary.Common/DictionaryConstants.cs
+++ src/Common/BlazorDictionary.Common/DictionaryConstants.cs
@@ -6,5 +6,18 @@ public class DictionaryConstants
     public const string DefaultExchangeType = "direct";
 
     public const string UserExhangeName = "UserExchange";
+    public const string FavExhangeName = "FavExhange";
+    public const string VoteExhangeName = "VoteExhange";
+
     public const string UserEmailChangedQueueName = "UserEmailChangedQueue";
+
+    public const string CreateEntryCommentFavQueueName = "CreateEntryCommentFavQueue";
+    public const string CreateEntryCommentVoteQueueName = "CreateEntryCommentVoteQueue";
+    public const string CreateEntryFavQueueName = "CreateEntryFavQueue";
+    public const string CreateEntryVoteQueueName = "CreateEntryVoteQueue";
+
+    public const string DeleteEntryFavQueueName = "DeleteEntryFavQueue";
+    public const string DeleteEntryVoteQueueName = "DeleteEntryVoteQueue";
+    public const string DeleteEntryCommentFavQueueName = "DeleteEntryCommentFavQueue";
+    public const string DeleteEntryCommentVoteQueueName = "DeleteEntryCommentVoteQueue";
 }
