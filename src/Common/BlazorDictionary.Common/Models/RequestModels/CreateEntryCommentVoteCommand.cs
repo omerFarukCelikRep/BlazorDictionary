423470ef1cf021deddede120bd626e50146a6f8e
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 01:03:13 +0300

    BlazorDictionary.Common.Models.RequestModels CreateEntryCommentVoteCommand added

---
+++ src/Common/BlazorDictionary.Common/Models/RequestModels/CreateEntryCommentVoteCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Common.Models.RequestModels;
+
+public class CreateEntryCommentVoteCommand : IRequest<bool>
+{
+    public CreateEntryCommentVoteCommand()
+    {
+
+    }
+    public CreateEntryCommentVoteCommand(Guid entryCommentId, VoteType voteType, Guid createdBy)
+    {
+        EntryCommentId = entryCommentId;
+        VoteType = voteType;
+        CreatedBy = createdBy;
+    }
+
+    public Guid EntryCommentId { get; set; }
+    public VoteType VoteType { get; set; }
+    public Guid CreatedBy { get; set; }
+}
