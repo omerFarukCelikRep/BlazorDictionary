7796eb950ca3c17d22c699b136025aedcca1bd04
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:40:59 +0300

    BlazorDictionary.Api.Application.Features.Commands.EntryComments.CreateFav CreateEntryCommentFavCommandHandler added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/EntryComments/CreateFav/CreateEntryCommentFavCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using BlazorDictionary.Common;
+using BlazorDictionary.Common.Events.EntryComments;
+using BlazorDictionary.Common.Infrastracture;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.EntryComments.CreateFav;
+
+public class CreateEntryCommentFavCommandHandler : IRequestHandler<CreateEntryCommentFavCommand, bool>
+{
+    public async Task<bool> Handle(CreateEntryCommentFavCommand request, CancellationToken cancellationToken)
+    {
+        QueueFactory.SendMessageToExhange(exchangeName: DictionaryConstants.FavExhangeName, 
+                                            exchangeType: DictionaryConstants.DefaultExchangeType, 
+                                            queueName: DictionaryConstants.CreateEntryCommentFavQueueName,
+                                            obj: new CreateEntryCommentFavEvent()
+                                            {
+                                                EntryCommentId = request.EntryCommentId,
+                                                CreatedBy = request.UserId
+                                            });
+
+        return await Task.FromResult(true);
+    }
+}
