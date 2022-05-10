0be6f321af6ea386ce9bbb8d1f75fe6615ee785b
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:47:47 +0300

    BlazorDictionary.Api.Application.Features.Commands.EntryComments.DeleteVote DeleteEntryCommentVoteCommandHandler added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/EntryComments/DeleteVote/DeleteEntryCommentVoteCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using BlazorDictionary.Common;
+using BlazorDictionary.Common.Events.EntryComments;
+using BlazorDictionary.Common.Infrastracture;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.EntryComments.DeleteVote;
+
+public class DeleteEntryCommentVoteCommandHandler : IRequestHandler<DeleteEntryCommentVoteCommand, bool>
+{
+    public async Task<bool> Handle(DeleteEntryCommentVoteCommand request, CancellationToken cancellationToken)
+    {
+        QueueFactory.SendMessageToExhange(exchangeName: DictionaryConstants.VoteExhangeName,
+                                            exchangeType: DictionaryConstants.DefaultExchangeType,
+                                            queueName: DictionaryConstants.DeleteEntryCommentVoteQueueName,
+                                            obj: new DeleteEntryCommentVoteEvent()
+                                            {
+                                                EntryCommentId = request.EntryCommentId,
+                                                CreatedBy = request.UserId
+                                            });
+
+        return await Task.FromResult(true);
+    }
+}
