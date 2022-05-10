f9d21224fa7074afceb5f32032f8cd19cff896be
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:48:39 +0300

    BlazorDictionary.Api.Application.Features.Commands.Users.ConfirmEmail ConfirmEmailCommandHandler added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/Users/ConfirmEmail/ConfirmEmailCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using BlazorDictionary.Api.Application.Interfaces.Repositories;
+using BlazorDictionary.Common.Infrastracture.Exceptions;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.Users.ConfirmEmail;
+
+public class ConfirmEmailCommandHandler : IRequestHandler<ConfirmEmailCommand, bool>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IEmailConfirmationRepository _emailConfirmationRepository;
+
+    public ConfirmEmailCommandHandler(IUserRepository userRepository, IEmailConfirmationRepository emailConfirmationRepository)
+    {
+        _userRepository = userRepository;
+        _emailConfirmationRepository = emailConfirmationRepository;
+    }
+    public async Task<bool> Handle(ConfirmEmailCommand request, CancellationToken cancellationToken)
+    {
+        var confirmation = await _emailConfirmationRepository.GetByIdAsync(request.ConfirmationId);
+
+        if (confirmation == null)
+        {
+            throw new DatabaseValidationException("Confirmation Not Found");
+        }
+
+        var dbUser = await _userRepository.GetSingleAsync(x => x.Email == confirmation.NewEmailAddress);
+
+        if (dbUser == null)
+        {
+            throw new DatabaseValidationException("User Not Found with This Email");
+        }
+
+        if (dbUser.EmailConfirmed)
+        {
+            throw new DatabaseValidationException("Email is already confirmed!");
+        }
+
+        dbUser.EmailConfirmed = true;
+
+        return await _userRepository.UpdateAsync(dbUser) > 0;
+    }
+}
