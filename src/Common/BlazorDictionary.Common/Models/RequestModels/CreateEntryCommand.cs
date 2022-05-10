0dab5388f06697f3a68bbbb467f52affb985ffc2
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 01:02:35 +0300

    BlazorDictionary.Common.Models.RequestModels CreateEntryCommand added

---
+++ src/Common/BlazorDictionary.Common/Models/RequestModels/CreateEntryCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Common.Models.RequestModels;
+
+public class CreateEntryCommand : IRequest<Guid>
+{
+    public CreateEntryCommand()
+    {
+
+    }
+    public CreateEntryCommand(string subject, string content, Guid? createdById)
+    {
+        Subject = subject;
+        Content = content;
+        CreatedById = createdById;
+    }
+
+    public string Subject { get; set; }
+    public string Content { get; set; }
+    public Guid? CreatedById { get; set; }
+}
