7b32d3ccea8221e621db59d38d994fc1f41406fa
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 01:01:33 +0300

    BlazorDictionary.Common.Events.EntryComments DeleteEntryCommentFavEvent added

---
+++ src/Common/BlazorDictionary.Common/Events/EntryComments/DeleteEntryCommentFavEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorDictionary.Common.Events.EntryComments;
+
+public class DeleteEntryCommentFavEvent
+{
+    public Guid EntryCommentId { get; set; }
+    public Guid CreatedBy { get; set; }
+}
