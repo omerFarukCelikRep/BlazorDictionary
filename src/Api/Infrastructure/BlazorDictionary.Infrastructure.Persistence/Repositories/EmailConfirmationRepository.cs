113cf35c2b5f1b9e396d9493053dc68c88570626
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:56:29 +0300

    BlazorDictionary.Infrastructure.Persistence.Repositories EmailConfirmationRepository added

---
+++ src/Api/Infrastructure/BlazorDictionary.Infrastructure.Persistence/Repositories/EmailConfirmationRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlazorDictionary.Api.Application.Interfaces.Repositories;
+using BlazorDictionary.Api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorDictionary.Infrastructure.Persistence.Repositories;
+
+public class EmailConfirmationRepository : BaseRepository<EmailConfirmation>, IEmailConfirmationRepository
+{
+    public EmailConfirmationRepository(DbContext context) : base(context) { }
+}
