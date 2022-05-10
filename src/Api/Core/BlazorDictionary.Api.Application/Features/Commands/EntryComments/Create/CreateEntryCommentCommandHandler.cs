ca036a63c7e2655c0b6155b68159652e19cf28e8
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:40:31 +0300

    BlazorDictionary.Api.Application.Features.Commands.EntryComments.Create CreateEntryCommentCommandHandler added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Commands/EntryComments/Create/CreateEntryCommentCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BlazorDictionary.Api.Application.Interfaces.Repositories;
+using BlazorDictionary.Api.Domain.Models;
+using BlazorDictionary.Common.Models.RequestModels;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.EntryComments.Create;
+
+public class CreateEntryCommentCommandHandler : IRequestHandler<CreateEntryCommentCommand, Guid>
+{
+    private readonly IEntryCommentRepository _entryCommentRepository;
+    private readonly IMapper _mapper;
+
+    public CreateEntryCommentCommandHandler(IEntryCommentRepository entryCommentRepository, IMapper mapper)
+    {
+        _entryCommentRepository = entryCommentRepository;
+        _mapper = mapper;
+    }
+    public async Task<Guid> Handle(CreateEntryCommentCommand request, CancellationToken cancellationToken)
+    {
+        var dbEntryComment = _mapper.Map<EntryComment>(request);
+
+        await _entryCommentRepository.AddAsync(dbEntryComment);
+
+        return dbEntryComment.Id;
+    }
+}
