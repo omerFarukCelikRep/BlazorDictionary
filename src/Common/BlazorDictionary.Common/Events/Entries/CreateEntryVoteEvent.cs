453ce6ff22e1974bd4b11bb467a25ee58d7ea113
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 00:59:07 +0300

    BlazorDictionary.Common.Events.Entries CreateEntryVoteEvent added

---
+++ src/Common/BlazorDictionary.Common/Events/Entries/CreateEntryVoteEvent.cs
@@ -0,0 +1,10 @@
+﻿using BlazorDictionary.Common.Models;
+
+namespace BlazorDictionary.Common.Events.Entries;
+
+public class CreateEntryVoteEvent
+{
+    public Guid EntryId { get; set; }
+    public VoteType VoteType { get; set; }
+    public Guid CreatedBy { get; set; }
+}
