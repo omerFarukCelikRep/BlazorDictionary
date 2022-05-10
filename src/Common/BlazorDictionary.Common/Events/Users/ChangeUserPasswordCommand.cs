2cd7b85deda4a836bc40748fe6f0734a5892538a
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-11 01:02:22 +0300

    BlazorDictionary.Common.Events.Users ChangeUserPasswordCommand added

---
+++ src/Common/BlazorDictionary.Common/Events/Users/ChangeUserPasswordCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Common.Events.Users;
+
+public class ChangeUserPasswordCommand : IRequest<bool>
+{
+    public ChangeUserPasswordCommand(Guid? userId, string oldPassword, string newPassword)
+    {
+        UserId = userId;
+        OldPassword = oldPassword;
+        NewPassword = newPassword;
+    }
+
+    public Guid? UserId { get; set; }
+    public string OldPassword { get; set; }
+    public string NewPassword { get; set; }
+}
