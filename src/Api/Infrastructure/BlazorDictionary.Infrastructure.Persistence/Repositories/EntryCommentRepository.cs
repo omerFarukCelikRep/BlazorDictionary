5ff76f7d85bbb375100bb708bd5a68a14de65eec
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-15 19:33:52 +0300

    constructor context type changed

--- src/Api/Infrastructure/BlazorDictionary.Infrastructure.Persistence/Repositories/EntryCommentRepository.cs
+++ src/Api/Infrastructure/BlazorDictionary.Infrastructure.Persistence/Repositories/EntryCommentRepository.cs
@@ -1,10 +1,10 @@
 ﻿using BlazorDictionary.Api.Application.Interfaces.Repositories;
 using BlazorDictionary.Api.Domain.Models;
-using Microsoft.EntityFrameworkCore;
+using BlazorDictionary.Infrastructure.Persistence.Contexts;
 
 namespace BlazorDictionary.Infrastructure.Persistence.Repositories;
 
 public class EntryCommentRepository : BaseRepository<EntryComment>, IEntryCommentRepository
 {
-    public EntryCommentRepository(DbContext context) : base(context) { }
+    public EntryCommentRepository(BlazorDictionaryDbContext context) : base(context) { }
 }
