d3f483c82d90d370b8c51c36fbef1bddd624b564
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-15 19:35:09 +0300

    BasePagedQuery added

---
+++ src/Common/BlazorDictionary.Common/Models/Pages/BasePagedQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorDictionary.Common.Models.Pages;
+
+public class BasePagedQuery
+{
+    public BasePagedQuery(int page, int pageSize)
+    {
+        Page = page;
+        PageSize = pageSize;
+    }
+
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+}
