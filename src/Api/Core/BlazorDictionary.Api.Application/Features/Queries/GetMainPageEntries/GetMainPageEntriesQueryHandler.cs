76be01daa6570fc61c2dd3f8c5e7f6472eda941d
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-15 19:31:48 +0300

    GetMainPageEntriesQueryHandler added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Queries/GetMainPageEntries/GetMainPageEntriesQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using BlazorDictionary.Api.Application.Interfaces.Repositories;
+using BlazorDictionary.Common.Infrastracture.Extensions;
+using BlazorDictionary.Common.Models.Pages;
+using BlazorDictionary.Common.Models.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorDictionary.Api.Application.Features.Queries.GetMainPageEntries;
+
+public class GetMainPageEntriesQueryHandler : IRequestHandler<GetMainPageEntriesQuery, PagedViewModel<GetEntryDetailsViewModel>>
+{
+    private readonly IEntryRepository _entryRepository;
+
+    public GetMainPageEntriesQueryHandler(IEntryRepository entryRepository)
+    {
+        _entryRepository = entryRepository;
+    }
+    public async Task<PagedViewModel<GetEntryDetailsViewModel>> Handle(GetMainPageEntriesQuery request, CancellationToken cancellationToken)
+    {
+        var query = _entryRepository.AsQueryable();
+
+        query.Include(x => x.EntryFavorites)
+             .Include(x => x.CreatedBy)
+             .Include(x => x.EntryVotes);
+
+        var entryDetails = query.Select(x => new GetEntryDetailsViewModel
+        {
+            Id = x.Id,
+            Subject = x.Subject,
+            Content = x.Content,
+            IsFavorited = request.UserId.HasValue && x.EntryFavorites.Any(x => x.CreatedById == request.UserId),
+            FavoritedCount = x.EntryFavorites.Count,
+            CreatedDate = x.CreateDate,
+            CreatedByUserName = x.CreatedBy.Username,
+            VoteType = request.UserId.HasValue && x.EntryVotes.Any(x => x.CreatedById == request.UserId)
+                                    ? x.EntryVotes.FirstOrDefault(x => x.CreatedById == request.UserId).VoteType
+                                    : Common.Models.VoteType.None
+        });
+
+        var entries = await entryDetails.GetPaged(request.Page, request.PageSize);
+
+        return new PagedViewModel<GetEntryDetailsViewModel>(entries.Results, entries.PageInfo);
+    }
+}
