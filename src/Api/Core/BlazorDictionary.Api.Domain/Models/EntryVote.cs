a886c9a022aff64c978f1c29edaaef046343d0b0
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-15 19:32:46 +0300

    code cleanup

--- src/Api/Core/BlazorDictionary.Api.Domain/Models/EntryVote.cs
+++ src/Api/Core/BlazorDictionary.Api.Domain/Models/EntryVote.cs
@@ -6,7 +6,6 @@ namespace BlazorDictionary.Api.Domain.Models
     {
         public VoteType VoteType { get; set; }
         public Guid CreatedById { get; set; }
-
         public Guid EntryId { get; set; }
         public virtual Entry Entry { get; set; }
     }
