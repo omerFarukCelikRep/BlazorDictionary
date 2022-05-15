6cadae671d9ccd1ea879e2c2f747ed2eaa8b0c5c
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-15 19:35:38 +0300

    BaseFooterFavoritedViewModel added

---
+++ src/Common/BlazorDictionary.Common/Models/Queries/BaseFooterFavoritedViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorDictionary.Common.Models.Queries;
+
+public class BaseFooterFavoritedViewModel
+{
+    public bool IsFavorited { get; set; }
+    public int FavoritedCount { get; set; }
+}
