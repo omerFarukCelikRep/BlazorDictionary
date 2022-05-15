38f5106291d35363b709327fca08851a100e1f82
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-15 19:31:39 +0300

    GetMainPageEntriesQuery added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Queries/GetMainPageEntries/GetMainPageEntriesQuery.cs
@@ -0,0 +1,15 @@
+﻿using BlazorDictionary.Common.Models.Pages;
+using BlazorDictionary.Common.Models.Queries;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Queries.GetMainPageEntries;
+
+public class GetMainPageEntriesQuery : BasePagedQuery, IRequest<PagedViewModel<GetEntryDetailsViewModel>>
+{
+    public GetMainPageEntriesQuery(Guid? userId, int page, int pageSize) : base(page, pageSize)
+    {
+        UserId = userId;
+    }
+
+    public Guid? UserId { get; set; }
+}
