4a67ad56d280a06babed0a6ebc2af966a7a60285
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-15 19:35:30 +0300

    PagedViewModel added

---
+++ src/Common/BlazorDictionary.Common/Models/Pages/PagedViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorDictionary.Common.Models.Pages;
+
+public class PagedViewModel<T> where T : class
+{
+    public PagedViewModel() : this(new List<T>(), new Page()) { }
+    public PagedViewModel(IList<T> results, Page pageInfo)
+    {
+        Results = results;
+        PageInfo = pageInfo;
+    }
+
+    public IList<T> Results { get; set; }
+    public Page PageInfo { get; set; }
+}
