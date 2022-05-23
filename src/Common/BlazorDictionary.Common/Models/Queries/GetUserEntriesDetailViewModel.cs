3b8dee4c3f2ddbbcd0098c75ebbfef09db7b00c1
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-23 11:17:05 +0300

    GetUserEntriesDetailViewModel added

---
+++ src/Common/BlazorDictionary.Common/Models/Queries/GetUserEntriesDetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorDictionary.Common.Models.Queries;
+public class GetUserEntriesDetailViewModel : BaseFooterFavoritedViewModel
+{
+    public Guid Id { get; set; }
+    public string Subject { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedByUserName { get; set; }
+}
