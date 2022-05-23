20f7c2a1ce7c424888cce7ff9882dc099b01a3fc
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-23 11:17:14 +0300

    SearchEntryQuery added

---
+++ src/Common/BlazorDictionary.Common/Models/Queries/SearchEntryQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BlazorDictionary.Common.Models.Queries;
+public class SearchEntryQuery : IRequest<List<SearchEntryViewModel>>
+{
+    public SearchEntryQuery()
+    {
+
+    }
+    public SearchEntryQuery(string searchText)
+    {
+        SearchText = searchText;
+    }
+
+    public string SearchText { get; set; }
+}
