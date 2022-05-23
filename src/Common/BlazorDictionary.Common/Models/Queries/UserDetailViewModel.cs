137521736678cac84ba2e1147bbdea1b8e8ce95a
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-23 11:17:33 +0300

    UserDetailViewModel added

---
+++ src/Common/BlazorDictionary.Common/Models/Queries/UserDetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorDictionary.Common.Models.Queries;
+public class UserDetailViewModel
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public bool EmailConfirmed { get; set; }
+}
