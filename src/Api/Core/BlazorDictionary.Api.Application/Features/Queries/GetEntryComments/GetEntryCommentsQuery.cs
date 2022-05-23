48e620a31fa8187bdf78e93843f8c36b6fe31442
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-23 11:09:26 +0300

    GetEntryCommentsQuery added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Queries/GetEntryComments/GetEntryCommentsQuery.cs
@@ -0,0 +1,16 @@
+﻿using BlazorDictionary.Common.Models.Pages;
+using BlazorDictionary.Common.Models.Queries;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Queries.GetEntryComments;
+public class GetEntryCommentsQuery : BasePagedQuery, IRequest<PagedViewModel<GetEntryCommentsViewModel>>
+{
+    public GetEntryCommentsQuery(Guid entryId, Guid? userId, int page, int pageSize) : base(page, pageSize)
+    {
+        EntryId = entryId;
+        UserId = userId;
+    }
+
+    public Guid EntryId { get; set; }
+    public Guid? UserId { get; set; }
+}
