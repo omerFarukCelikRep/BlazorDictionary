a00f80377a042d2fd3f46cd9fb3deb92be6db9cc
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-23 11:12:26 +0300

    GetUserEntriesQuery added

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Features/Queries/GetUserEntries/GetUserEntriesQuery.cs
@@ -0,0 +1,16 @@
+﻿using BlazorDictionary.Common.Models.Pages;
+using BlazorDictionary.Common.Models.Queries;
+using MediatR;
+
+namespace BlazorDictionary.Api.Application.Features.Queries.GetUserEntries;
+public class GetUserEntriesQuery : BasePagedQuery, IRequest<PagedViewModel<GetUserEntriesDetailViewModel>>
+{
+    public GetUserEntriesQuery(Guid? userId, string? userName = null, int page = 1, int pageSize = 10) : base(page, pageSize)
+    {
+        UserId = userId;
+        UserName = userName;
+    }
+
+    public Guid? UserId { get; set; }
+    public string? UserName { get; set; }
+}
