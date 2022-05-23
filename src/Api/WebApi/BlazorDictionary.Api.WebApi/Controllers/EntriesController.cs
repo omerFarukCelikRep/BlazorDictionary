1781ecb2c6c93dcd864c760876568089f66439c7
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-23 11:14:22 +0300

    GetById, GetEntryComments, GetUserEntries and Search actions added

--- src/Api/WebApi/BlazorDictionary.Api.WebApi/Controllers/EntriesController.cs
+++ src/Api/WebApi/BlazorDictionary.Api.WebApi/Controllers/EntriesController.cs
@@ -1,13 +1,15 @@
 ﻿using BlazorDictionary.Api.Application.Features.Queries.GetEntries;
+using BlazorDictionary.Api.Application.Features.Queries.GetEntryComments;
+using BlazorDictionary.Api.Application.Features.Queries.GetEntryDetail;
 using BlazorDictionary.Api.Application.Features.Queries.GetMainPageEntries;
+using BlazorDictionary.Api.Application.Features.Queries.GetUserEntries;
+using BlazorDictionary.Common.Models.Queries;
 using BlazorDictionary.Common.Models.RequestModels;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorDictionary.Api.WebApi.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
 public class EntriesController : BaseController
 {
     private readonly IMediator _mediator;
@@ -25,12 +27,49 @@ public class EntriesController : BaseController
         return Ok(entries);
     }
 
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var result = await _mediator.Send(new GetEntryDetailQuery(id, UserId));
+
+        return Ok(result);
+    }
+
     [HttpGet("MainPageEntries")]
     public async Task<IActionResult> GetMainPageEntries(int page, int pageSize)
     {
         var entries = await _mediator.Send(new GetMainPageEntriesQuery(UserId, page, pageSize));
 
         return Ok(entries);
+    }
+
+    [HttpGet("Comments/{id:guid}")]
+    public async Task<IActionResult> GetEntryComments(Guid id, int page, int pageSize)
+    {
+        var result = await _mediator.Send(new GetEntryCommentsQuery(id,UserId,page, pageSize));
+
+        return Ok(result);
+    }
+
+    [HttpGet("UserEntries")]
+    public async Task<IActionResult> GetUserEntries(string userName, Guid userId, int page, int pageSize)
+    {
+        if (userId == Guid.Empty && string.IsNullOrEmpty(userName))
+        {
+            userId = UserId.Value;
+        }
+
+        var result = await _mediator.Send(new GetUserEntriesQuery(userId, userName, page, pageSize));
+
+        return Ok(result);
+    }
+
+    [HttpGet("Search")]
+    public async Task<IActionResult> Search([FromQuery] SearchEntryQuery query)
+    {
+        var result = await _mediator.Send(query);
+
+        return Ok(result);
     }
 
     [HttpPost]
