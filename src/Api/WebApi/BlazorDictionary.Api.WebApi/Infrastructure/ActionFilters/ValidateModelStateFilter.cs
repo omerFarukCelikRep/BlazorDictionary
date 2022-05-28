2aebb96a6b827b21fad5309b1701b4ba75d0dd8c
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-28 19:09:23 +0300

    OnActionExecutionAsync method added

---
+++ src/Api/WebApi/BlazorDictionary.Api.WebApi/Infrastructure/ActionFilters/ValidateModelStateFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BlazorDictionary.Api.WebApi.Infrastructure.ActionFilters;
+
+public class ValidateModelStateFilter : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (context.ModelState.IsValid)
+        {
+            await next();
+        }
+
+        var messages = context.ModelState.Values.SelectMany(x => x.Errors)
+                                                .Select(x => !string.IsNullOrEmpty(x.ErrorMessage) ? x.ErrorMessage : x.Exception?.Message)
+                                                .Distinct()
+                                                .ToList();
+
+        return;
+    }
+}
