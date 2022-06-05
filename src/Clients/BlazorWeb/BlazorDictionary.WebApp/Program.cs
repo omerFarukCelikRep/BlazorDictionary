6df0ac8108cbd5023750c1a143f1f8b10df54f8c
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-05 16:49:03 +0300

    client and service registrations added

--- src/Clients/BlazorWeb/BlazorDictionary.WebApp/Program.cs
+++ src/Clients/BlazorWeb/BlazorDictionary.WebApp/Program.cs
@@ -1,4 +1,7 @@
 using BlazorDictionary.WebApp;
+using BlazorDictionary.WebApp.Infrastructure.Services;
+using BlazorDictionary.WebApp.Infrastructure.Services.Interfaces;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,24 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("WebApiClient", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["WebApiClient:Url"]);
+});
+
+builder.Services.AddScoped(serviceProvider =>
+{
+    var clientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+
+    return clientFactory.CreateClient("WebApiClient");
+});
+
+builder.Services.AddTransient<IVoteService, VoteService>();
+builder.Services.AddTransient<IEntryService, EntryService>();
+builder.Services.AddTransient<IFavoriteService, FavoriteService>();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IIdentityService, IdentityService>();
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
