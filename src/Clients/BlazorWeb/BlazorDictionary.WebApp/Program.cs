914240b63140f6ba019469d9bb1415288cabf2e4
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-12 21:18:40 +0300

    HttpMessageHandler and AuthorizationCore added

--- src/Clients/BlazorWeb/BlazorDictionary.WebApp/Program.cs
+++ src/Clients/BlazorWeb/BlazorDictionary.WebApp/Program.cs
@@ -1,7 +1,9 @@
 using BlazorDictionary.WebApp;
+using BlazorDictionary.WebApp.Infrastructure.Authorization;
 using BlazorDictionary.WebApp.Infrastructure.Services;
 using BlazorDictionary.WebApp.Infrastructure.Services.Interfaces;
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,7 +14,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddHttpClient("WebApiClient", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["WebApiClient:Url"]);
-});
+})
+.AddHttpMessageHandler<AuthTokenHandler>();
 
 builder.Services.AddScoped(serviceProvider =>
 {
@@ -27,6 +30,11 @@ builder.Services.AddTransient<IFavoriteService, FavoriteService>();
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IIdentityService, IdentityService>();
 
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+builder.Services.AddScoped<AuthTokenHandler>();
+
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
