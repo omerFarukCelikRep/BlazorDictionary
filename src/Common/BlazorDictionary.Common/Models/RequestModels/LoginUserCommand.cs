614737f210a18bac96febf029f3eb5b188bf4de6
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-12 21:19:51 +0300

    refactoring

--- src/Common/BlazorDictionary.Common/Models/RequestModels/LoginUserCommand.cs
+++ src/Common/BlazorDictionary.Common/Models/RequestModels/LoginUserCommand.cs
@@ -5,8 +5,8 @@ namespace BlazorDictionary.Common.Models.RequestModels;
 
 public class LoginUserCommand : IRequest<LoginUserViewModel>
 {
-    public string Email { get; private set; }
-    public string Password { get; private set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 
     public LoginUserCommand(string email, string password)
     {
