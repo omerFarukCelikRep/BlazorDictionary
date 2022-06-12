f517fa451d2ad67ede09a459b2b019d6df144773
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-12 21:10:42 +0300

    Authorize attribute added

--- src/Api/WebApi/BlazorDictionary.Api.WebApi/Controllers/BaseController.cs
+++ src/Api/WebApi/BlazorDictionary.Api.WebApi/Controllers/BaseController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 namespace BlazorDictionary.Api.WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class BaseController : ControllerBase
 {
     public Guid? UserId => new(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
